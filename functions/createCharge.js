cdc7d287472c16529bd9f29172ead4d10d58c2fe
Whitman Schorn <wschorn@quartethealth.com>
2019-06-25 15:40:12 -0400

    create charge with customer record

--- functions/createCharge.js
+++ functions/createCharge.js
@@ -1,18 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-module.exports.handler = (event, context, callback) => {
-  console.log('createCharge');
-  console.log(event);
-  const requestBody = JSON.parse(event.body);
-  console.log(requestBody);
-
-  const token = requestBody.token.id;
-  const amount = requestBody.charge.amount;
-  const currency = requestBody.charge.currency;
-  const email = requestBody.charge.email;
-  const name = requestBody.charge.name;
-
-  return stripe.charges.create({ // Create Stripe charge with token
+const createCharge = (customerId, onComplete, { amount, currency, token, email, name }) => {
+  stripe.charges.create({ // Create Stripe charge with token
     amount,
     currency,
     description: 'Serverless Stripe Test charge',
@@ -31,7 +20,66 @@ module.exports.handler = (event, context, callback) => {
           charge,
         }),
       };
-      callback(null, response);
+      onComplete(null, response);
+    })
+    .catch((err) => { // Error response
+      console.log(err);
+      const response = {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({
+          error: err.message,
+        }),
+      };
+      onComplete(null, response);
+    });
+};
+
+
+module.exports.handler = (event, context, callback) => {
+  console.log('createCharge');
+  console.log(event);
+  const requestBody = JSON.parse(event.body);
+  console.log(requestBody);
+
+  const token = requestBody.token.id;
+  const amount = requestBody.charge.amount;
+  const currency = requestBody.charge.currency;
+  const email = requestBody.charge.email;
+  const name = requestBody.charge.name;
+
+
+  // create or find customer
+
+  stripe.customers.list({ limit: 1, email })
+    .then((customers) => {
+      const cust = customers[0];
+      console.log('found customer', cust);
+      if (cust && cust.id) {
+        return createCharge(cust.id, callback, { amount, currency, token, email, name });
+      }
+      // no customer found, so we create one
+      stripe.customers.create({ email, name })
+        .then((newCustomer) => {
+          if (newCustomer && newCustomer.id) {
+            return createCharge(newCustomer.id, callback, { amount, currency, token, email, name });
+          }
+        })
+        .catch((err) => { // Error response
+          console.log(err);
+          const response = {
+            statusCode: 500,
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify({
+              error: err.message,
+            }),
+          };
+          callback(null, response);
+        });
     })
     .catch((err) => { // Error response
       console.log(err);
