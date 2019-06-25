8e098ee4633ed9eb4ad476d6c38c03254cc490a7
Whitman Schorn <wschorn@quartethealth.com>
2019-06-25 18:39:20 -0400

    file

---
+++ functions/createUser.js
@@ -0,0 +1,52 @@
+const ManagementClient = require('auth0').ManagementClient;
+
+module.exports.handler = (event, context, callback) => {
+  console.log('createUser');
+  const requestBody = JSON.parse(event.body);
+  console.log(requestBody);
+
+  const email = requestBody.user.email;
+
+  const management = new ManagementClient({
+    domain: 'dev-wschorn.auth0.com',
+    clientId: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    scope: 'create:users',
+  });
+
+  const data = {
+    connection: 'email',
+    email,
+    email_verified: true,
+  };
+  management.createUser(data, (err) => {
+    if (err) {
+    // Handle error.
+      console.log(err);
+      const response = {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({
+          error: err.message,
+        }),
+      };
+      callback(null, response);
+    }
+  // User created.
+    console.log({ data });
+    data.password = null;
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        message: 'User created succesfully!',
+        data,
+      }),
+    };
+    callback(null, response);
+  });
+};
